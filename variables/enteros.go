37a1cda62bb88932b769fbb733b015ddf01e5d64
Randolfjoan <66329775+Randolfjoan@users.noreply.github.com>
2024-12-30 13:21:01 -0400

    Primer commit

---
+++ variables/enteros.go
@@ -0,0 +1,11 @@
+package variables
+
+import "fmt"
+
+func Muestroenteros() {
+	var x int
+	x = 42
+	intde32 := int32(10)
+	intde64 := int64(100)
+	fmt.Println("x es = ", x, "intde32 es = ", intde32, "intde64 es = ", intde64)
+}
